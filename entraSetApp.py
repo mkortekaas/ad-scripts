bea17dd60161e1b06bed45c899ec72dd32ab7fe9
Mark Kortekaas <mark@kortekaas.com>
2024-09-12 11:17:48 -0600

    entra app testing

--- entraSetApp.py
+++ entraSetApp.py
@@ -29,11 +29,12 @@ import json
 import jwt
 import os
 import uuid
+import logging
 
 ###################################################################################
 def setPasswordSSO(app_id, app_name, service_principal_id, sso_url, headers):
     ## no idea how to make this work
-    print(f"Setting password-based SSO for {app_name} with URL {sso_url}")
+    print(f"\tSetting password-based SSO for {app_name} with URL {sso_url}")
 
     ### chatgpt recommended this but this does not work:
     # body = { "web": { "redirectUris": [] } }
@@ -54,26 +55,33 @@ def setPasswordSSO(app_id, app_name, service_principal_id, sso_url, headers):
     next_uri = f"{graph_api_url}/v1.0/servicePrincipals/{service_principal_id}"
     response = requests.patch(next_uri, headers=headers, json=body)
     if response.status_code == 204:
-        print(f"Application {app_name} is now set to password-based SSO")
+        print(f"\t{app_name} is now set to password-based SSO")
     else:
-        print(f"ERROR Failed to set SSO mode for application {app_name}")
-        print(f"URI: {next_uri}")
-        print(json.dumps(body, indent=4))
-        print(json.dumps(response.json(), indent=4))
-        exit (1);
+        logging.error(f"Failed to set SSO mode for application {app_name}")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(body, indent=4))
+        logging.error(json.dumps(response.json(), indent=4))
+        exit (1)
+
+###################################################################################
+def getAppUserAccessURL(app_name, app_id, service_principal_id, tenant_id, headers):
+    ## While I'd love to have this be a call, doesn't appear we can get this info from the API (??) but the pattern seems to always be:
+    user_access_url = f"https://launcher.myapps.microsoft.com/api/signin/{app_id}?tenantId={tenant_id}"
+    print(f"\taccess URL for {app_name}: {user_access_url}")
+    return user_access_url
 
 ###################################################################################
 def getAppUsersGroups(app_id, app_aid, app_name, service_principal_id, headers):
-    print(f"Getting Users/Groups assigned to {app_name}")
+    print(f"\tGetting Users/Groups assigned to {app_name}")
     next_uri = f"{graph_api_url}/v1.0/servicePrincipals/{service_principal_id}/appRoleAssignedTo"
     response = requests.get(
         next_uri,
         headers=headers,
     )
     if response.status_code != 200:
-        print("Failed to retrieve users/groups assigned to application")
-        print(f"URI: {next_uri}")
-        print(json.dumps(response.json(), indent=4))
+        logging.error("Failed to retrieve users/groups assigned to application")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)
 
     response_json = response.json()
@@ -85,7 +93,7 @@ def getAppUsersGroups(app_id, app_aid, app_name, service_principal_id, headers):
 
 
 ###################################################################################
-def addAppRole(app_name, app_id, app_details, headers, DEBUG): 
+def addAppRole(app_name, app_id, app_details, headers): 
     ## this works
     appRoleUUID = str(uuid.uuid4())
     new_app_role = {
@@ -104,11 +112,11 @@ def addAppRole(app_name, app_id, app_details, headers, DEBUG):
         json={"appRoles": app_details["appRoles"]}
     )
     if response.status_code == 204:
-        print(f"New appRole added to application {app_name}")
+        print(f"\tNew appRole added to application {app_name}")
     else:
-        print(f"ERROR Failed to add new appRole to application {app_name}")
-        print(f"URI: {next_uri}")
-        print(json.dumps(response.json(), indent=4))
+        logging.error(f"Failed to add new appRole to application {app_name}")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)
     return appRoleUUID
 
@@ -118,9 +126,9 @@ def getAppDetails(app_name, headers):
     next_uri = f"{graph_api_url}/v1.0/applications?$filter=displayName eq '{app_name}'"
     response = requests.get(next_uri, headers=headers)
     if response.status_code != 200:
-        print("Failed to retrieve application information")
-        print(f"URI: {next_uri}")
-        print(json.dumps(response.json(), indent=4))
+        logging.error("Failed to retrieve application information")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)
 
     app_info = response.json()
@@ -136,20 +144,19 @@ def getAppDetails(app_name, headers):
     next_uri = f"{graph_api_url}/v1.0/applications/{app_id}"
     response = requests.get(next_uri, headers=headers)
     if response.status_code != 200:
-        print("ERROR Failed to retrieve detailed application information")
-        print(f"URI: {next_uri}")
-        print(json.dumps(response.json(), indent=4))
+        logging.error("Failed to retrieve detailed application information")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)
 
     app_details = response.json()
-    if DEBUG:
-        print("============================== DEBUG APP_DETAILS =======================================")
-        print(json.dumps(app_details, indent=4))
-        print("============================== END DEBUG APP_DETAILS =======================================")
+    logging.debug("============================== DEBUG APP_DETAILS =======================================")
+    logging.debug(json.dumps(app_details, indent=4))
+    logging.debug("============================== END DEBUG APP_DETAILS =======================================")
     return app_info, app_details, app_id, app_aid
 
 ###################################################################################
-def getToken(tenant_id, client_id, client_secret, DEBUG):
+def getToken(tenant_id, client_id, client_secret):
     ## this works
     app = msal.ConfidentialClientApplication(
         client_id,
@@ -158,25 +165,23 @@ def getToken(tenant_id, client_id, client_secret, DEBUG):
     )
     result = app.acquire_token_for_client(scopes=required_scopes)
     if "access_token" not in result:
-        print("Failed to acquire token")
-        print(result.get("error"))
-        print(result.get("error_description"))
+        logging.error("Failed to acquire token")
+        logging.error(result.get("error"))
+        logging.error(result.get("error_description"))
         exit()
 
     access_token = result["access_token"]
-
-    if DEBUG:
-        print("============================== DEBUG JWT =======================================")
-        decoded_token = jwt.decode(access_token, options={"verify_signature": False})
-        print(json.dumps(decoded_token, indent=4))
-        scopes = decoded_token.get("scp", "No scopes found")
-        print(f"Scopes: {scopes}")
-        print("============================== END DEBUG JWT =======================================")
+    logging.debug("============================== DEBUG JWT =======================================")
+    decoded_token = jwt.decode(access_token, options={"verify_signature": False})
+    logging.debug(json.dumps(decoded_token, indent=4))
+    scopes = decoded_token.get("scp", "No scopes found")
+    logging.debug(f"Scopes: {scopes}")
+    logging.debug("============================== END DEBUG JWT =======================================")
 
     return access_token
 
 ###################################################################################
-def addGroupToApp(group_name, app_name, app_id, app_aid, appRoleUUID, service_principal_id, headers, DEBUG):
+def addGroupToApp(group_name, app_name, app_id, app_aid, appRoleUUID, service_principal_id, headers):
     ## This works
     next_uri = f"{graph_api_url}/v1.0/groups?$filter=displayName eq '{group_name}'"
     response = requests.get(
@@ -184,9 +189,9 @@ def addGroupToApp(group_name, app_name, app_id, app_aid, appRoleUUID, service_pr
         headers=headers
     )
     if response.status_code != 200:
-        print("ERROR: Failed to retrieve group information")
-        print(f"URI: {next_uri}")
-        print(json.dumps(response.json(), indent=4))
+        logging.error("Failed to retrieve group information")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(response.json(), indent=4))
         exit (1)
 
     group_info = response.json()
@@ -210,43 +215,42 @@ def addGroupToApp(group_name, app_name, app_id, app_aid, appRoleUUID, service_pr
         json=add_group_body
     )
     if response.status_code == 201:
-        print(f"Group {group_name} added to application {app_name}")
+        print(f"\tGroup {group_name} added to application {app_name}")
     else:
-        print(f"ERROR Failed to add group {group_name} to application {app_name}")
-        print(f"URI: {next_uri}")
-        print(json.dumps(add_group_body, indent=4))
-        print(json.dumps(response.json(), indent=4))
+        logging.error(f"Failed to add group {group_name} to application {app_name}")
+        logging.error(f"URI: {next_uri}")
+        logging.error(json.dumps(add_group_body, indent=4))
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)
     return group_id
 
 ###################################################################################
-def getAppServicePrincipal(app_name, headers, DEBUG):
+def getAppServicePrincipal(app_name, headers):
     ## this works
     service_principal_url = f"{graph_api_url}/v1.0/servicePrincipals?$filter=displayName eq '{app_name}'"
     response = requests.get(service_principal_url, headers=headers)
     if response.status_code != 200:
-        print("Failed to retrieve service principal information")
-        print(json.dumps(response.json(), indent=4))
+        logging.error("Failed to retrieve service principal information")
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)
 
-    if DEBUG:
-        print("============================== DEBUG APP SVC PRINCIPAL =======================================")
-        print(json.dumps(response.json(), indent=4))
-        print("============================== END DEBUG APP SVC PRINCIPAL =======================================")
+    logging.debug("============================== DEBUG APP SVC PRINCIPAL =======================================")
+    logging.debug(json.dumps(response.json(), indent=4))
+    logging.debug("============================== END DEBUG APP SVC PRINCIPAL =======================================")
 
     service_principal_info = response.json()
     if not service_principal_info["value"]:
-        print(f"Service principal for application {app_name} not found")
+        logging.error(f"Service principal for application {app_name} not found")
         exit(1)
 
     # Extract the service principal ID
     service_principal_id = service_principal_info["value"][0]["id"]
-    print(f"Service principal ID for application {app_name} is {service_principal_id}")
+    print(f"\tService principal ID for application {app_name} is {service_principal_id}")
     return service_principal_id
 
 ###################################################################################
-def updateGroupCredentials(group_id, appRoleUUID, service_principal_id, principal_display_name, username, password, headers, DEBUG):
-    print(f"Updating credentials for group {group_id} for the {principal_display_name} user")
+def updateGroupCredentials(group_id, appRoleUUID, service_principal_id, principal_display_name, username, password, headers):
+    print(f"\tUpdating credentials for group {group_id} for the {principal_display_name} user")
     ### THIS NO WORKING
     credentials = {
         "principalId": group_id,
@@ -262,13 +266,12 @@ def updateGroupCredentials(group_id, appRoleUUID, service_principal_id, principa
     
     if response.status_code == 200:
         print(f"Credentials updated for group {group_id}")
-        if DEBUG:
-            print("============================== DEBUG UPDATED CREDENTIALS =======================================")
-            print(json.dumps(response.json(), indent=4))
-            print("============================== END DEBUG UPDATED CREDENTIALS =======================================")
+        logging.debug("============================== DEBUG UPDATED CREDENTIALS =======================================")
+        logging.debug(json.dumps(response.json(), indent=4))
+        logging.debug("============================== END DEBUG UPDATED CREDENTIALS =======================================")
     else:
-        print(f"ERROR Failed to update credentials for group {group_id}")
-        print(json.dumps(response.json(), indent=4))
+        logging.error(f"Failed to update credentials for group {group_id}")
+        logging.error(json.dumps(response.json(), indent=4))
         exit(1)    
 
 ###################################################################################
@@ -279,7 +282,7 @@ parser = argparse.ArgumentParser(description="Set up an application for password
 parser.add_argument("--tenant_id", help="Azure AD tenant ID", default=os.getenv("TENANT_ID"))
 parser.add_argument("--client_id", help="Azure AD client ID", default=os.getenv("CLIENT_ID"))
 parser.add_argument("--client_secret", help="Azure AD client secret", default=os.getenv("CLIENT_SECRET"))
-parser.add_argument("--DEBUG", help="Enable debug output", action="store_true", default=True)
+parser.add_argument("--DEBUG", help="Enable debug output", action="store_true", default=False)
 parser.add_argument("--app_name", help="Application name", default=os.getenv("APP_NAME"))
 parser.add_argument("--sso_url", help="SSO URL", default=os.getenv("SSO_URL"))
 parser.add_argument("--group_name", help="GROUP NAME", default=os.getenv("GROUP_NAME"))
@@ -297,7 +300,13 @@ app_name = args.app_name
 sso_url = args.sso_url
 group_name = args.group_name
 
-access_token = getToken(tenant_id, client_id, client_secret, DEBUG)
+## set this to DEBUG if you want more details from the https request calls
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+else:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+access_token = getToken(tenant_id, client_id, client_secret)
 headers = {
     "Authorization": f"Bearer {access_token}",
     "Content-Type": "application/json",
@@ -305,9 +314,9 @@ headers = {
 
 # basic information about the app && enterprise app (aka: service principal)
 (app_info, app_details, app_id, app_aid) = getAppDetails(app_name, headers)
-service_principal_id = getAppServicePrincipal(app_name, headers, DEBUG)
+service_principal_id = getAppServicePrincipal(app_name, headers)
 
-setPasswordSSO(app_id, app_name, service_principal_id, sso_url, headers)
+user_access_url = getAppUserAccessURL(app_name, app_id, service_principal_id, tenant_id, headers)
 
 ## we have already done this a bunch of times - just hardcoding during testing
 appRoleUUID = addAppRole(app_name, app_id, app_details, headers, DEBUG)
@@ -324,5 +333,5 @@ password = "new_password"
 for item in assigned_users_groups:
     # item is a json object with the following keys we can use: id, deletedDateTime, appRoleId, createdDateTime, principalDisplayName, principalId, principalType, resourceDisplayName, resourceId
     principal_display_name = item.get("principalDisplayName")
-    updateGroupCredentials(group_id, appRoleUUID, service_principal_id, principal_display_name, username, password, headers, DEBUG)
+    updateGroupCredentials(group_id, appRoleUUID, service_principal_id, principal_display_name, username, password, headers)
 
